f2b54456ccd654264f965e58add55e50a338fb08
Meghsham Kapure <153221564+code-meghsham@users.noreply.github.com>
2023-12-16 01:42:34 +0530

    Second Commit

---
+++ Addition.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main (){
+    int a;
+    int b;
+
+    printf("Give me 2 Number to add");
+
+    printf("\nEnter A");
+    scanf("%d",&a);
+    printf("\nEnter B");
+    scanf("%d",&b);
+    c=a+b;
+    printf ("%d",c);
+}
